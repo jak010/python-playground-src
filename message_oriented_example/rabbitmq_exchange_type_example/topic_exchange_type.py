4b03b4c94f74b44790988558191b28850351fc4b
jak010 <bluetoon@naver.com>
2023-09-09 19:02:43 +0900

    add:topic_exchange_type

--- message_oriented_example/rabbitmq_exchange_type_example/topic_exchange_type.py
+++ message_oriented_example/rabbitmq_exchange_type_example/topic_exchange_type.py
@@ -1,27 +1,26 @@
 import pika
+from pika.exchange_type import ExchangeType
 
 # RabbitMQ 서버에 연결
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
 channel = connection.channel()
 
 # Exchange 설정 (exchange_type='topic')
 exchange_name = 'amq.topic'
-channel.exchange_declare(exchange=exchange_name, exchange_type='topic', durable=True)
+
+channel.exchange_declare(exchange=exchange_name, exchange_type=ExchangeType.topic, durable=True)
 
 # 메시지 보내기 (exchange와 routing_key 설정)
 
-routing_key = 'a.b.(#)'  # 라우팅 키 설정
-while True:
-    import time
+routing_key = 'a.b'  # 라우팅 키 설정
 
-    time.sleep(1)
+channel.basic_publish(
+    exchange=exchange_name,
+    routing_key=routing_key,
+    body=b'Topic Message',
+)
 
-    channel.basic_publish(
-        exchange=exchange_name,
-        routing_key=routing_key,
-        body='Topic Message'
-    )
-
-    print(f"Sent '' with routing key '{routing_key}' to '{exchange_name}'")
+print(f"Sent '' with routing key '{routing_key}' to '{exchange_name}'")
 
 connection.close()
